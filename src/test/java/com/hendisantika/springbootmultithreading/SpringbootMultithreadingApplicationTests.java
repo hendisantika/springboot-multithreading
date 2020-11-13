133d413aa8954635bf13ff0ea5d3fd41cd4102c2
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-13 21:48:54 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/hendisantika/springbootmultithreading/SpringbootMultithreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootmultithreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMultithreadingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
