8a333fecd6b2519a8e53c9556d5041bd84708a71
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-14 08:26:37 +0700

    #9 - Add parseCSVFile method in User Service

--- src/main/java/com/hendisantika/springbootmultithreading/service/UserService.java
+++ src/main/java/com/hendisantika/springbootmultithreading/service/UserService.java
@@ -8,6 +8,10 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -45,6 +49,27 @@ public class UserService {
         log.info("get list of user by " + Thread.currentThread().getName());
         List<User> users = repository.findAll();
         return CompletableFuture.completedFuture(users);
+    }
+
+    private List<User> parseCSVFile(final MultipartFile file) throws Exception {
+        final List<User> users = new ArrayList<>();
+        try {
+            try (final BufferedReader br = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    final String[] data = line.split(",");
+                    final User user = new User();
+                    user.setName(data[0]);
+                    user.setEmail(data[1]);
+                    user.setGender(data[2]);
+                    users.add(user);
+                }
+                return users;
+            }
+        } catch (final IOException e) {
+            log.error("Failed to parse CSV file {}", e);
+            throw new Exception("Failed to parse CSV file {}", e);
+        }
     }
 
 }
