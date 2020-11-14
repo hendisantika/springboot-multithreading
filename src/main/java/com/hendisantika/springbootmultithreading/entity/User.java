4ef832fc528372954a3fcaddc3a9f437dd3c567c
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-14 08:21:16 +0700

    #4 - Add User entity

---
+++ src/main/java/com/hendisantika/springbootmultithreading/entity/User.java
@@ -0,0 +1,33 @@
+package com.hendisantika.springbootmultithreading.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-multithreading
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/11/20
+ * Time: 08.19
+ */
+@Data
+@Entity
+@Table(name = "USERS")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String email;
+    private String gender;
+}
