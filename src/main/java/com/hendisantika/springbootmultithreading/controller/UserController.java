e6249326f0e508fcc39bfe19b82a7c6b2f661c0d
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-14 08:53:32 +0700

    #12 - Add saveUsers API in UserController

--- src/main/java/com/hendisantika/springbootmultithreading/controller/UserController.java
+++ src/main/java/com/hendisantika/springbootmultithreading/controller/UserController.java
@@ -2,7 +2,13 @@ package com.hendisantika.springbootmultithreading.controller;
 
 import com.hendisantika.springbootmultithreading.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,4 +23,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private UserService service;
+
+    @PostMapping(value = "/users", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = "application/json")
+    public ResponseEntity saveUsers(@RequestParam(value = "files") MultipartFile[] files) throws Exception {
+        for (MultipartFile file : files) {
+            service.saveUsers(file);
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
