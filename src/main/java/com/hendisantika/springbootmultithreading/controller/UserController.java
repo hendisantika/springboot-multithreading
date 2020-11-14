ee5819b84ba40aeee93007eb4f403ef0cdc392cf
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-14 08:53:46 +0700

    #13 - Add findAllUsers API in UserController

--- src/main/java/com/hendisantika/springbootmultithreading/controller/UserController.java
+++ src/main/java/com/hendisantika/springbootmultithreading/controller/UserController.java
@@ -5,10 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,4 +34,10 @@ public class UserController {
         }
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    @GetMapping(value = "/users", produces = "application/json")
+    public CompletableFuture<ResponseEntity> findAllUsers() {
+        return service.findAllUsers().thenApply(ResponseEntity::ok);
+    }
+
 }
